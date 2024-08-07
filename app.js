bf077c477838efc44e6ae8d293dac479b8ae328a
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2024-08-07 21:51:20 +0530

    full basic todo complete with CRUD

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Todo = require("./models/todo");
+const methodOverride = require("method-override");
 
 const con = async () => {
   mongoose.connect("mongodb://127.0.0.1:27017/mytodo");
@@ -16,6 +17,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
   const todos = await Todo.find({});
@@ -23,12 +26,36 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/todo/new", (req, res) => {
-  res.render("new.ejs");
+  res.render("./todo/new.ejs");
 });
 
 app.post("/todo", async (req, res) => {
   const todo = new Todo(req.body);
   await todo.save();
+  res.redirect("/");
+});
+
+app.patch("/todo/:id/completed", async (req, res) => {
+  const { id } = req.params;
+  await Todo.findByIdAndUpdate(id, { completed: true });
+  res.redirect("/");
+});
+
+app.delete("/todo/:id/delete", async (req, res) => {
+  const { id } = req.params;
+  await Todo.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+app.get("/todo/:id/edit", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  res.render("./todo/edit.ejs", { todo });
+});
+
+app.put("/todo/:id/update", async (req, res) => {
+  const { id } = req.params;
+  const todo = req.body;
+  await Todo.findByIdAndUpdate(id, { todo: todo.todo });
   res.redirect("/");
 });
 
--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema({
-  todo: String,
+  todo: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  // owner: {
+  //   type: mongoose.Schema.ObjectId,
+  // },
 });
 
 const Todo = new mongoose.model("Todo", todoSchema);
